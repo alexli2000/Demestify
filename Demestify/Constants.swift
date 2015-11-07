031156896644094d86ea2a986d281cfe43068ffb
alexli2000 <alex@Alexanders-MacBook-Pro.local>
2015-11-07 09:32:55 -0500

    Add Parse

---
+++ Demestify/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Demestify
+//
+//  Created by Alexander Li on 2015-11-07.
+//  Copyright © 2015 Alexander Li. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+let PF_APPLICATIONKEY = "vyuW0y1a9mLqnNLbbmVjDDv0BKGheERXKUEVhA0y"
+let PF_CLIENTKEY = "u08uNsd4KvsgGWt4wxWxMYPm4vb8xLhhQqENpuI9"
