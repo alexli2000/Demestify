d371047f98fed2f4a864eb01fa62819c8cfe1bd9
alexli2000 <alex@Alexanders-MacBook-Pro.local>
2015-11-07 12:46:15 -0500

    Add shop files

---
+++ Demestify/StoreViewController.swift
@@ -0,0 +1,40 @@
+//
+//  StoreViewController.swift
+//  Demestify
+//
+//  Created by Alexander Li on 2015-11-07.
+//  Copyright © 2015 Alexander Li. All rights reserved.
+//
+
+import UIKit
+
+class StoreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func purchaseClothing() {
+        
+    }
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
